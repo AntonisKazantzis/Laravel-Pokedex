e33fa52dfb609898fa04efc174976a7c7d875cb0
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-02-21 03:18:03 +0200

    feat: styling

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,15 +6,14 @@ import Welcome from '@/Components/Welcome.vue';
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
+            <div class="flex justify-between text-white dark:text-black p-[10px]">
+                <h2 class="font-semibold text-xl flex-grow flex items-center justify-start">Dashboard</h2>
+            </div>
         </template>
 
-        <div class="py-12">
+        <div class="py-12 border-t">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
                 </div>
             </div>
         </div>
