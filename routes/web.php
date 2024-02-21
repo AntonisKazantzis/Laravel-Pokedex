3242a346986b7d0b488a6f541295a32a662290a1
Lucifer <118682784+TheIightbringer@users.noreply.github.com>
2024-02-21 03:19:57 +0200

    feat: add pokemon routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\PokemonController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,4 +33,9 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
+    Route::resource('/pokemons', PokemonController::class)->only('index','show');
+
+    Route::post('/like/{pokemon}', [PokemonController::class, 'like'])->name('pokemons.like');
+    Route::get('/user/pokemons', [PokemonController::class, 'liked'])->name('pokemons.liked');
 });
